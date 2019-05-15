3e776e25429e2de1ba71f96ba2dfe7ed0c833a8a
karolkt1 <32594640+karolkt1@users.noreply.github.com>
2019-05-15 16:52:34 +0200

    Firebase support
    Added firebase support via cloud firestore (not realtime db)
    Added phonecall function which selects phone number

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -2,31 +2,31 @@ var textToRead;
 
 
 function popup() {
-    textToRead = prompt("Co mam przeczytać", "");
+  textToRead = prompt("Co mam przeczytać", "");
 
-    if (textToRead != null) {
-      document.getElementById("readme").innerHTML =
+  if (textToRead != null) {
+    document.getElementById("readme").innerHTML =
       "Zostanie przeczytana następująca wiadomość: " + textToRead;
-    }
-  };
-  
+  }
+};
+
 function speak() {
   speechSynthesis.speak(new SpeechSynthesisUtterance(textToRead));
 };
 
-function speakAPI(){
-  responsiveVoice.speak(textToRead,"Polish Female");
+function speakAPI() {
+  responsiveVoice.speak(textToRead, "Polish Female");
 };
 
 
-function vibration(){
-navigator.vibrate(3000);
+function vibration() {
+  navigator.vibrate(1000);
 }
 
 var watchID = null;
 
 
-function gyro(){
+function gyro() {
   startWatch();
 }
 function startWatch() {
@@ -39,20 +39,63 @@ function startWatch() {
 
 function stopWatch() {
   if (watchID) {
-      navigator.accelerometer.clearWatch(watchID);
-      watchID = null;
+    navigator.accelerometer.clearWatch(watchID);
+    watchID = null;
   }
 }
 
 function onSuccess(acceleration) {
   var element = document.getElementById('accelerometer');
-  element.innerHTML = 'Acceleration X: ' + acceleration.x         + '<br />' +
-                      'Acceleration Y: ' + acceleration.y         + '<br />' +
-                      'Acceleration Z: ' + acceleration.z         + '<br />' +
-                      'Timestamp: '      + acceleration.timestamp + '<br />';
+  element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+    'Acceleration Y: ' + acceleration.y + '<br />' +
+    'Acceleration Z: ' + acceleration.z + '<br />' +
+    'Timestamp: ' + acceleration.timestamp + '<br />';
 }
 
 
 function onError() {
   alert('onError!');
 }
+
+function phoneCall() {
+  var numerAlarmowy = "900300400";
+  location.href = "tel:+" + numerAlarmowy;
+
+}
+
+
+// Here begins firebase code
+
+var db;
+var firebaseConfig = {
+  apiKey: "AIzaSyBP8Un_hWobMbOE5HgU41Cwne7L7vMTVhw",
+  authDomain: "bogate-aplikacje-internetowe.firebaseapp.com",
+  databaseURL: "https://bogate-aplikacje-internetowe.firebaseio.com",
+  projectId: "bogate-aplikacje-internetowe",
+  storageBucket: "bogate-aplikacje-internetowe.appspot.com",
+  messagingSenderId: "860894210224",
+  appId: "1:860894210224:web:b950180e478fe509"
+};
+
+// Initialize Firebase
+var app = firebase.initializeApp(firebaseConfig);
+db = firebase.firestore(app);
+
+var textVar = document.getElementById("fbText");
+var submitButton = document.getElementById("submitButton");
+
+
+// Legacy code for realtime database 
+// function submitClick2() {
+//   var rootRef = firebase.database().ref();
+//   rootRef.child("Text").set("This is a test value");
+// }
+
+function submitClick() {
+  db.collection("Users").doc("Data").set({
+    email: "kowalski@uek.krakow.pl",
+    name: "Jan",
+    phoneNumber: "500500500"
+  })
+}
+
