8269118f08f8d3f6c0741833ccc03ce2530a5ad2
karolkt1 <32594640+karolkt1@users.noreply.github.com>
2019-05-25 21:11:51 +0200

    Local Storage functionality

--- Calendar/js/scripts.js
+++ Calendar/js/scripts.js
@@ -21,18 +21,11 @@ function addDate() {
     userTime = document.getElementById("time1").value;
     event1 = new Event(userText, userDate, userTime);
     myJSON = JSON.stringify(event1);
-    resultBeta();
-    fillCallendar();
+    fillCalendar();
 }
 
-
-function resultBeta() {
-    var result = document.getElementById("result");
-    result.innerHTML = "\n To jest JSON object " + myJSON
-}
-
-// new code, it will probably break the app
 var tbl = document.getElementById("table1");
+
 function buildTable() {
     var tbl = document.getElementById("table1");
     if (tbl.rows.length > 1) {
@@ -54,16 +47,50 @@ function buildTable() {
 }
 buildTable();
 
-function fillCallendar() {
+function fillCalendar() {
     var userHour = parseInt(userTime);
     if (userHour > 12) {
         userHour = userHour - 12;
     } else if (userHour == 0) {
         userHour = userHour + 1;
     }
-    tableOfEvents[userHour-1]=event1;
-   
+    tableOfEvents[userHour - 1] = event1;
+
     tbl.rows[userHour].cells[1].innerHTML = userText;
+    // This line is extremely important because it stores events in localStorage 
+    storeObject(tableOfEvents);
 }
 
+function storeObject(param1) {
+    // Put the object into storage
+    localStorage.setItem('storedObject', JSON.stringify(param1));
+}
+
+function getStoredObject() {
+    // Retrieve the object from storage
+    var retrievedObject = localStorage.getItem('storedObject');
+    return retrievedObject;
+}
+
+var parsedJSON;
+function jsonToEvent(jsonFile) {
+    parsedJSON = JSON.parse(jsonFile);
+    return parsedJSON;
+}
+
+// Get tableOfEvents if stored in localStorage
+var magicFormula
+function rebuildCalendar() {
+    magicFormula = jsonToEvent(getStoredObject());
+    for (var x = 0; x < magicFormula.length; x++) {
+        if (magicFormula[x]) {
+            // tbl.rows[parseInt(magicFormula[x].userTime)].cells[1].innerHTML = magicFormula[x].userMessage;
+            tbl.rows[x + 1].cells[1].innerHTML = magicFormula[x].userMessage;
+        }
+    }
+    console.log(magicFormula);
+}
+function clearLocalStorage() {
+    localStorage.clear();
+}
 // custom alert features, pre-mobile version
