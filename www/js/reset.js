93b99b7aaa9fce6ef4f1e61024a91cec65ec8117
Jarosław Starmach <32633257+Nixoor@users.noreply.github.com>
2019-05-28 13:27:06 +0200

    Add files via upload
    Dodanie funkcji resetu hasła

--- www/js/reset.js
+++ www/js/reset.js
@@ -1,8 +1,15 @@
-
-var emailadrr = document.getElementById("emailr").value;
-
-auth.sendPasswordResetEmail(emailadrr).then(function() {
-  console.log('poszedł mail');
-}).catch(function(error) {
-  console.log('problem');
-});
+function resetPass(){
+	var email = document.getElementById('emailreset').value;
+	firebase.auth().sendPasswordResetEmail(email).then(function() {
+		alert('Password Reset Email Sent!');
+	}).catch(function(error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		if (errorCode == 'auth/invalid-email') {
+			alert(errorMessage);
+		} else if (errorCode == 'auth/user-not-found') {
+			alert(errorMessage);
+		}
+		console.log(error);
+	});
+}
