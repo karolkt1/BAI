7e8afb687717016304f169df061d0449398a2872
Jarosław Starmach <32633257+Nixoor@users.noreply.github.com>
2019-05-30 15:08:58 +0200

    Add files via upload
    js do zmiany hasła update

--- www/js/evidence.js
+++ www/js/evidence.js
@@ -11,3 +11,30 @@
 	})
 	console.log("data sent")
 	}
+
+//funkcja sprawdzająca poprawnosc podanego hasła w polu currentpass	
+	
+function currentClick() {
+var user = firebase.auth().currentUser;
+var currentpass = document.getElementById("currentpass").value="";
+var credential = firebase.auth.EmailAuthProvider.credential(
+  firebase.auth().currentUser.email,
+  currentpass
+);
+
+//następnie jeżeli hasełko się zgadza to jest możliwość jego zmiany
+
+user.reauthenticateWithCredential(credential).then(function() {
+var user = firebase.auth().currentUser;
+var resetpass = document.getElementById("resetpass").value="";
+
+user.updatePassword(resetpass).then(function() {
+  alert("Nastąpiła zmiana hasła");
+  
+}).catch(function(error) {
+  alert(error);
+});
+}).catch(function(error) {
+  alert("Sprawdź czy podane hasło jest poprawne");
+});
+}
